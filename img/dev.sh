64d6d5a4f922edccb69f7640a398d37b49e275b4
wacdev <i@wac.tax>
2023-04-11 08:42:51 +0800

    ♨

--- img/dev.sh
+++ img/dev.sh
@@ -5,7 +5,7 @@ cd $DIR
 set -ex
 
 export RUSTFLAGS='--cfg reqwest_unstable'
-export RUST_LOG=$RUST_LOG,watchexec_cli=off
+export RUST_LOG=$RUST_LOG,watchexec=off,watchexec_cli=off
 
 exec watchexec --shell=none \
   --project-origin . -w ./src \
--- img/src/main.rs
+++ img/src/main.rs
@@ -3,43 +3,54 @@
 mod env;
 mod err;
 mod img;
-mod log;
 mod root;
 mod urlmap;
 
 use std::{net::SocketAddr, time::Duration};
 
-use axum::{error_handling::HandleErrorLayer, http::StatusCode, routing::get, BoxError, Router};
+use axum::{
+  body::{Body, BoxBody, Bytes},
+  error_handling::HandleErrorLayer,
+  extract::MatchedPath,
+  http::{HeaderMap, Request, StatusCode},
+  response::{Html, Response},
+  routing::get,
+  BoxError, Router,
+};
 use env::env_default;
-use time::macros::format_description;
-use tower::ServiceBuilder;
-use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, TraceLayer};
-use tracing::{info, instrument, Level};
+use time::{macros::format_description, UtcOffset};
+use tower::{util::AndThenLayer, ServiceBuilder};
+use tower_http::{
+  classify::ServerErrorsFailureClass,
+  trace::{DefaultMakeSpan, DefaultOnRequest, TraceLayer},
+};
+use tracing::{error, info, info_span, instrument, Level, Span};
 use tracing_subscriber::{
   fmt,
   fmt::{format::FmtSpan, time::UtcTime},
+  layer::SubscriberExt,
   prelude::__tracing_subscriber_SubscriberExt,
   util::SubscriberInitExt,
   EnvFilter,
 };
 
-use crate::root::root;
-
+use crate::{fmt::time::OffsetTime, root::root};
 const TIMEOUT: u64 = 600;
 
 #[tokio::main]
 async fn main() {
-  let timer = UtcTime::new(format_description!(
-    "[year][month][day]>[hour]:[minute]:[second]"
-  ));
+  let local_time = OffsetTime::new(
+    UtcOffset::from_hms(8, 0, 0).unwrap(),
+    format_description!("[year][month][day] [hour][minute][second] >"),
+  );
 
   tracing_subscriber::registry()
-    .with(fmt::layer().with_timer(timer)
+        .with(fmt::layer().with_timer(local_time)
             .with_level(false))
-             // .with_file(true)
-            // .with_line_number(true)
-    .with(EnvFilter::from_default_env())
-    .init();
+        // .with_file(true)
+        // .with_line_number(true)
+        .with(EnvFilter::from_default_env())
+        .init();
 
   let addr = SocketAddr::from(([0, 0, 0, 0], env_default("PORT", 9911)));
   info!("http://{}", addr);
@@ -56,10 +67,34 @@ async fn main() {
         ))
       }
     }))
+    .layer(
+      TraceLayer::new_for_http()
+        .make_span_with(|request: &Request<_>| {
+          // Log the matched route's path (with placeholders not filled in).
+          // Use request.uri() or OriginalUri if you want the real path.
+
+          let method = request.method().to_string();
+          let uri = request.uri().to_string();
+          info_span!("", method = method, uri = uri)
+        })
+        .on_request(|_request: &Request<_>, _span: &Span| {
+          // You can use `_span.record("some_other_field", value)` in one of these
+          // closures to attach a value to the initially empty field in the info_span
+          // created above.
+          info!("\n> {:?}\n", _request.headers())
+        })
+        .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+          // ...
+          info!("\n{:?} {:?}\n", _latency, _response)
+        })
+        .on_failure(
+          |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+            error!("{:?} {:?}", _error, _span)
+          },
+        ),
+    )
     .timeout(Duration::from_secs(TIMEOUT))
-    .layer(ServiceBuilder::new().layer(
-      TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::new().include_headers(true)),
-    ))
+    .layer(ServiceBuilder::new())
     .into_inner();
 
   let router = urlmap!(Router::new());
